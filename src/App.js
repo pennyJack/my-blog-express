dabfa8b05bc773c34a98001d6a6b65b6bbe66bd4
Andre Wibbeke <andre.wibbeke@gmail.com>
2019-12-19 08:14:45 +0100

    feat: add additional pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AboutMe from './pages/AboutMe';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
+        <div id="page-body">
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/about-me">
+            <AboutMe />
+          </Route>
+          <Route path="/articles-list">
+            <ArticlesList />
+          </Route>
+          <Route path="/article">
+            <ArticlePage />
+          </Route>
+        </div>
       </div>
     </Router>
   );
---
+++ src/pages/AboutMe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <>
+            <h1>About me</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae eos provident nesciunt ea officia enim animi commodi dignissimos. Quisquam qui voluptate quos assumenda nam recusandae, sequi ipsa odio nesciunt repellat.
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat illo, voluptatum, nihil quidem porro accusantium iste enim minus, officiis mollitia possimus ad hic amet nemo modi voluptatibus deserunt optio. Cum?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aut placeat maiores esse quibusdam quas consequuntur sed minus corrupti? Quas, minus? Facere quo et consequuntur magni reprehenderit blanditiis placeat voluptates!
+            </p>
+        </>
+    );
+}
+
+export default AboutMe;
