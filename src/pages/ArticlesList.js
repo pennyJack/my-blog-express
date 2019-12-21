7755eafbf99072c289f5ec5da45e3883b1680c02
Andre Wibbeke <andre.wibbeke@gmail.com>
2019-12-21 16:25:01 +0100

    feat: add Links to articles with article preview

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import articleContent from './article-content';
 
 const ArticlesList = () => {
     return (
         <>
             <h1>Articles</h1>
+            {articleContent.map((article, index) => (
+                <Link className="article-list-item" key={index} to={`/article/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 151)}...</p>
+                </Link>
+            ))}
         </>
     );
 }
