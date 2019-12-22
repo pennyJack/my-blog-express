0992b1d32c4155a52216566fee7f43e5a7e14881
Andre Wibbeke <andre.wibbeke@gmail.com>
2019-12-22 17:37:51 +0100

    feat: add comments

--- server/src/server.js
+++ server/src/server.js
@@ -4,14 +4,17 @@ import bodyParser from 'body-parser';
 const articlesInfo = {
     'learn-react': {
         upvotes: 0,
+        comments: [],
     },
     'learn-node': {
         upvotes: 0,
+        comments: [],
     },
     'my-thoughts-on-resumes': {
         upvotes: 0,
+        comments: [],
     },
-}
+};
 
 const app = express();
 
@@ -21,12 +24,23 @@ app.use(bodyParser.json());
 // app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
 // app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 
+// Upvoting blog articles
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
 
     articlesInfo[articleName].upvotes += 1;
 
     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
+});
+
+// Adding comments
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].comments.push({ username, text });
+
+    res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
