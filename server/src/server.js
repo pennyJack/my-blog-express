43157406264b2ddc2beea05a7f6d01e058b8e922
Andre Wibbeke <andre.wibbeke@gmail.com>
2019-12-29 12:25:52 +0100

    refactor: send upvote to database

--- server/src/server.js
+++ server/src/server.js
@@ -25,12 +25,31 @@ app.get('/api/articles/:name', async (req, res) => {
 });
 
 // Upvoting blog articles
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const articleName = req.params.name;
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    try {
+        const articleName = req.params.name;
 
-    articlesInfo[articleName].upvotes += 1;
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
 
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        articleInfo.upvotes += 1;
+
+        await db.collection('articles').updateOne(
+            { name: articleName },
+            {
+                $set: { upvotes: articleInfo.upvotes },
+                $currentDate: { lastModified: true }
+            }
+        );
+
+        res.status(200).json(articleInfo);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
 });
 
 // Adding comments
