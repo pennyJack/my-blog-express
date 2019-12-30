b79a29f256e13bd9fd2cfa8481cdec5d18c5d3b2
Andre Wibbeke <andre.wibbeke@gmail.com>
2019-12-30 11:16:57 +0100

    refactor: send comment to database

--- server/src/server.js
+++ server/src/server.js
@@ -53,13 +53,32 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
 });
 
 // Adding comments
-app.post('/api/articles/:name/add-comment', (req, res) => {
-    const { username, text } = req.body;
-    const articleName = req.params.name;
+app.post('/api/articles/:name/add-comment', async (req, res) => {
+    try {
+        const { username, text } = req.body;
+        const articleName = req.params.name;
 
-    articlesInfo[articleName].comments.push({ username, text });
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
 
-    res.status(200).send(articlesInfo[articleName]);
+        let articleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        await db.collection('articles').updateOne(
+            { name: articleName },
+            {
+                $push: { comments: { username, text } },
+                $currentDate: { lastModified: true }
+            }
+        );
+
+        articleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        res.status(200).json(articleInfo);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
