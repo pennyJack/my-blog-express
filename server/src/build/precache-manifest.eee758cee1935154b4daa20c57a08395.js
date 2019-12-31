4fd59c943acc027843d7f651c59015ef7afc44a7
Andre Wibbeke <andre.wibbeke@gmail.com>
2019-12-31 15:48:41 +0100

    chore: remove /build folder from .gitignore

---
+++ server/src/build/precache-manifest.eee758cee1935154b4daa20c57a08395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45de2625a84a29c426d8067a4914f84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9f0e8b9a4b44bb8fd4e",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "22b9b5827d3c11bc8c4c",
+    "url": "/static/js/2.81a081d1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.81a081d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9f0e8b9a4b44bb8fd4e",
+    "url": "/static/js/main.8e0b2caf.chunk.js"
+  },
+  {
+    "revision": "6e55ea343e46b8691e3d",
+    "url": "/static/js/runtime-main.b5470c2c.js"
+  }
+]);
